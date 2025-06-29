d959128beaf2f309c9e435a8c20f73caf5ba3980
Tim Rid <6593626+timrid@users.noreply.github.com>
2025-06-29 21:45:10 +0200

    add minimal unit-test for BleakScannerChaquopy

--- examples/briefcase/tests/test_app.py
+++ examples/briefcase/tests/test_app.py
@@ -1,3 +1,16 @@
+import asyncio
+
+import pytest
+
+from bleak import BleakScanner
+
+
 def test_first():
     """An initial test for the app."""
     assert 1 + 1 == 2
+
+@pytest.mark.asyncio
+async def test_bleak_scanner():
+    async with BleakScanner() as scanner:
+        await asyncio.sleep(1)
+        assert len(scanner.discovered_devices_and_advertisement_data) == 0
