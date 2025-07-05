bcee7e84cd7c0cbd1ce9eb6dbbaa0dc68e599f9e
Tim Rid <6593626+timrid@users.noreply.github.com>
2025-07-05 19:40:32 +0200

    move my chaquopy to chaquopy_old

--- bleak/backends/client.py
+++ bleak/backends/client.py
@@ -3,6 +3,7 @@
 """
 Base class for backend clients.
 """
+
 import abc
 import os
 import platform
@@ -211,11 +212,11 @@ def get_platform_client_backend_type() -> type[BaseBleakClient]:
 
         return BleakClientP4Android
 
-    if platform.system() == "Android" and os.environ.get("CHAQUOPY_PROCESS_TYPE") is not None:
+    if os.environ.get("CHAQUOPY_PROCESS_TYPE") is not None:
         from bleak.backends.chaquopy.client import BleakClientChaquopy
 
         return BleakClientChaquopy
-    
+
     if platform.system() == "Linux":
         from bleak.backends.bluezdbus.client import BleakClientBlueZDBus
 
